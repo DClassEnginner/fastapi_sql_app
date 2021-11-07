f1b24203ca1e891ea8e082119193d20c48edda79
dClassEngineer <66309030+DClassEnginner@users.noreply.github.com>
2021-11-07 21:26:02 +0900

    add templates and HTMLResponse

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,6 +1,8 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
@@ -10,6 +12,8 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="sql_app/templates")
+
 # Dependency
 def get_db():
     db = SessionLocal()
@@ -18,6 +22,10 @@ def get_db():
     finally:
         db.close()
 
+@app.get("/", response_class=HTMLResponse)
+async def read_main(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
@@ -25,17 +33,17 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
-@app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+@app.get("/users/", response_class=HTMLResponse)
+def read_users(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+    return templates.TemplateResponse("users.html", {"request": request, "users": users})
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+@app.get("/users/{user_id}", response_class=HTMLResponse)
+def read_user(request: Request, user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    return templates.TemplateResponse("user.html", {"request": request, "user": db_user})
 
 @app.post("/users/{user_id}/items", response_model=schemas.Item)
 def create_item_for_user(
@@ -43,9 +51,9 @@ def create_item_for_user(
 ):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
-@app.get("/items", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+@app.get("/items", response_class=HTMLResponse)
+def read_items(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
-    return items
+    return templates.TemplateResponse("items.html", {"request": request, "items": items})
 
 
